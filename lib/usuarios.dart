d9f0939dfd65955daa5ead5fbacf04b87bd3bf7b
Estdimer <115674299+Estdimer@users.noreply.github.com>
2024-05-19 18:08:07 -0400

    a

--- lib/usuarios.dart
+++ lib/usuarios.dart
@@ -1,16 +1,15 @@
 
-
 class Usuario{
   int id;
   String nombre;
   String apellido;
-  Date fecha_nac;
+  //Date fecha_nac;
   String correo;
   String cont;
 
-  Usuario({this.id, this.nombre, this.apellido, this.fecha_nac, this.correo,this.cont});
+  Usuario({required this.id, required this.nombre, required this.apellido, /*this.fecha_nac,*/ required this.correo,required this.cont});
 
   Map<String, dynamic> toMap(){
-    return {'id':id,'nombre':nombre,'apellido':apellido,'fecha_nac':fecha_nac,'correo':correo,'cont':cont};
+    return {'id':id,'nombre':nombre,'apellido':apellido,/*'fecha_nac':fecha_nac,*/'correo':correo,'cont':cont};
   }
 }
